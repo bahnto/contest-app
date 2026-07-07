e66df3930d80c31f6a78aa2b36cb6ce83db5b795
Antonella Caldera <antonella_caldera@epam.com>
2026-07-07 12:02:00 -0300

    frutiger aero

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Inject floating bubbles into the page
+function Bubbles() {
+  return (
+    <div className="bubbles">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div key={i} className="bubble" />
+      ))}
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Bubbles />
       <App />
     </BrowserRouter>
   </React.StrictMode>
